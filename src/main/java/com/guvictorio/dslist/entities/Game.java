e47ff83643affa827f666f4f386e33bea8899299
GustavoVictorio <gu.victorio3@gmail.com>
2023-05-08 22:41:49 -0300

    Game seed

--- src/main/java/com/guvictorio/dslist/entities/Game.java
+++ src/main/java/com/guvictorio/dslist/entities/Game.java
@@ -9,18 +9,23 @@ import lombok.*;
 @Setter
 @EqualsAndHashCode
 @Entity
-@Table(name = "tb_games")
+@Table(name = "tb_game")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private Double score;
     @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
+
+    // title, score, game_year, genre, platforms, img_url, short_description, long_description
 
 }
