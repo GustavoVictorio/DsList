7283a295826d03be0a2de78161ff9e7554455793
GustavoVictorio <gu.victorio3@gmail.com>
2023-05-08 22:25:12 -0300

    Entity created

--- src/main/java/com/guvictorio/dslist/entities/Game.java
+++ src/main/java/com/guvictorio/dslist/entities/Game.java
@@ -1,2 +1,26 @@
-package com.guvictorio.dslist.entities;public class Game {
+package com.guvictorio.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_games")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
 }
